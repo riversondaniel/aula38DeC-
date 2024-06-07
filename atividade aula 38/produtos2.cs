b4db6f3a8db8d6d223e9ec95daca57eeb0d133ca
riverson nobre <riversonnobre@gmail.com>
2024-06-07 19:59:47 -0300

    ja declarei os atributos da class produtos2

---
+++ atividade aula 38/produtos2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade_aula_38
+{
+    class produtos2
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public id
+            publi
+
+    }
+}
